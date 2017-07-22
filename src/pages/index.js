e249d06d0eb48d496966957b49355b21e0634905
Kangsik Kevin Lee <kleekich@github.com>
2017-07-22 13:47:01 -0700

    alt tag added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
 export default class Index extends React.Component {
+  const alt = {
+    alt: 'Hi, Spider!',
+  };
+
+
   render() {
     return (
       <div itemscope itemtype="http://schema.org/FinancialProduct">
-        <h1>Hi spiders!</h1>
+        <h1 alt = {alt} >Hi spidersdfdf!</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
         <Link to="/page-2/">Go to page 2</Link>
