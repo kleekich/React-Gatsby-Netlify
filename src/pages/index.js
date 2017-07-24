9b800c37cb6914a867fddb5ea571c4d601be836b
eli <eli@meetbeam.com>
2017-07-24 03:41:47 -0700

    icon added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,29 @@ import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
 export default class Index extends React.Component {
+  const icons = [
+  {
+    header: 'ALL GROWTH',
+    subHeader: 'Get paid daily, in your sleep.',
+    icon: iconGrowth,
+  },
+  {
+    header: 'FDIC INSURED',
+    subHeader: 'Wall Street casinos? Never.',
+    icon: iconInsured,
+  },
+  {
+    header: 'NO LOCKUP',
+    subHeader: '24/7 access. $0 minimum.',
+    icon: iconLockUp,
+  },
+  {
+    header: 'ZERO FEES',
+    subHeader: 'Free means free. Forever.',
+    icon: iconFees,
+  },
+];
+
   render() {
     return (
       <div>
@@ -10,6 +33,56 @@ export default class Index extends React.Component {
         <p>Welcome to your new kang????!</p>
         <p>Now go build something great.</p>
         <Link to="/page-2/">Go to page 2</Link>
+         <Grid
+                columns={12}
+                css={{
+                  display: `block`,
+                  [presets.Tablet]: {
+                    display: `none`,
+                  },
+                }}
+              >
+                {icons.map((icon, i) => {
+                  return (
+                    <Span
+                      key={i}
+                      columns={4}
+                      squish={1}
+                      last={i % 2 ? true : false}
+                      css={{
+                        textAlign: `center`,
+                        marginBottom: rhythm(3 / 4),
+                      }}
+                    >
+                      <img
+                        src={icon.icon}
+                        css={{
+                          width: rhythm(2),
+                          marginBottom: rhythm(1 / 2),
+                        }}
+                      />
+                      <h3
+                        css={{
+                          ...scale(0),
+                          lineHeight: 1.3,
+                          marginBottom: rhythm(1 / 2),
+                        }}
+                      >
+                        {icon.header}
+                      </h3>
+                      <p
+                        css={{
+                          ...scale(-2 / 5),
+                          lineHeight: typography.options.baseLineHeight,
+                          color: colors.schist,
+                        }}
+                      >
+                        {icon.subHeader}
+                      </p>
+                    </Span>
+                  );
+                })}
+              </Grid>
       </div>
     )
   }
